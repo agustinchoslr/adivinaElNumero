be8eb6a894a1627ed056f3d5410cbd5ebd0da9fe
Agustin Juan <agustinchoslr@gmail.com>
2022-05-29 18:17:08 -0300

    subida

---
+++ js/adivinacionNumero.js
@@ -0,0 +1,20 @@
+let numIncognito = Math.round(Math.random() * 10);
+let numIngresado = parseInt(prompt("Adiviná el número que pensó la computadora"));
+let pista = numIngresado + numIncognito;
+
+if (numIngresado == numIncognito) {
+    document.write("<br>Felicitaciones, el número era " + numIncognito)
+}
+else {
+        var segundoIntentoVar = prompt(`número no acertado; te dejamos una pista \n resuelve el siguiente ejercicio matemático y lo adivinarás.\n A ${pista} restale ${numIngresado} y probalo acá:`)
+        
+    
+};
+
+
+if (segundoIntentoVar == numIncognito || numIngresado == numIncognito) {
+    document.write(`<br>SI, FELICITACIONES 🥳 EL NÚMERO ERA ${numIncognito}`)
+}
+else {
+    document.write("<br>No era ese, perdiste. Intenta de nuevo")
+};
---
+++ js/puntosFutbol.js
@@ -0,0 +1,19 @@
+// var cantVictorias = parseInt(prompt("ingrese cantidad de partidos ganados"));
+// var cantEmpates = parseInt(prompt("ingrese cantidad de partidos empatados"));
+// var totalPuntos = cantEmpates + (cantVictorias * 3);
+
+
+// document.write("<br><b>Su equipo obtuvo entonces</b> " + totalPuntos + " Puntos en total");
+
+// if (totalPuntos == 28) {
+//     document.write("<br>Su equipo está <i>igual que el año pasado en puntos 😐</i>")
+// };
+
+// if (totalPuntos < 28) {
+//     document.write("<br>su equipo está peor 😔 que el año pasado en cuanto a puntos")
+// };
+
+// if (totalPuntos > 28) {
+//     document.write("Bien! Su equipo viene mejor que el año pasado💪")
+// };
+
